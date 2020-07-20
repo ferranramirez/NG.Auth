2bd1f5999fcb9926a896c29477ad31331544481f
ferranramirez <ferranramireznavajon@gmail.com>
2020-07-20 22:38:37 +0200

    Update nugets and CORS

--- src/NG.Auth.Presentation.WebAPI/Startup.cs
+++ src/NG.Auth.Presentation.WebAPI/Startup.cs
@@ -49,7 +49,10 @@ namespace NG.Auth.Presentation.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors();
+            app.UseCors(builder => builder
+               .AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader());
 
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
